09ea918d37fbea1a688f2848b2d373a51cdc303b
maya-or-psyco <maya.pleau@gmail.com>
2017-07-19 15:33:08 -0400

    Add files via upload

---
+++ libs.js
@@ -0,0 +1,22 @@
+
+
+  function addlib() {
+  var thing1=document.getElementById("thingtext").value;
+  document.getElementById("thing").innerHTML = thing1;
+  }
+  function addlib2() {
+  var noun1=document.getElementById("thingtext").value;
+  document.getElementById("noun").innerHTML = noun1;
+  }
+
+
+  function addlib3() {
+  var sweet1=document.getElementById("thingtext").value;
+  document.getElementById("sweet").innerHTML = sweet1;
+  }
+
+
+
+
+document.getElementById("noun").innerHTML = noun;
+document.getElementById("sweet").innerHTML = sweet;
